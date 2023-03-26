f766986d6dccb3b1cfb7d009680eb8a33ee09743
jamesonsansone <38871626+jamesonsansone@users.noreply.github.com>
2023-03-26 11:33:47 -0400

    Update suite.py

--- suite.py
+++ suite.py
@@ -1,1 +1,40 @@
+pip install streamlit
 
+import os
+import pandas as pd
+import openai
+import streamlit as st
+
+# Include the generate_description function and other necessary functions from the previous code snippet
+
+os.environ["OPENAI_API_KEY"] = "your_api_key_here"
+
+st.title("Featured Snippet Optimization Tool")
+
+uploaded_file = st.file_uploader("Upload a CSV file containing keywords:", type=['csv'])
+
+if uploaded_file is not None:
+    keywords_df = pd.read_csv(uploaded_file)
+
+    if st.button("Generate Descriptions"):
+        st.write("Generating NLP-friendly descriptions for your keywords...")
+
+        descriptions = []
+
+        for keyword in keywords_df['keyword']:
+            description = generate_description(keyword)
+            descriptions.append(description)
+
+        keywords_df['description'] = descriptions
+
+        st.write(keywords_df)
+
+        st.download_button(
+            label="Download CSV with Descriptions",
+            data=keywords_df.to_csv(index=False).encode('utf-8'),
+            file_name="keywords_with_descriptions.csv",
+            mime="text/csv",
+        )
+        
+streamlit run suite.py
+        
