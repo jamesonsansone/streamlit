65d7e89b96220e162cfdc6a055009bd0fb523dab
jamesonsansone <38871626+jamesonsansone@users.noreply.github.com>
2023-03-30 22:14:24 -0400

    Update suite.py

--- suite.py
+++ suite.py
@@ -3,6 +3,11 @@ import pandas as pd
 import openai
 import streamlit as st
 
+
+
+
+
+
 # Function to generate description using OpenAI API
 def generate_description(keyword):
     openai.api_key = os.getenv("OPENAI_API_KEY")
@@ -31,23 +36,28 @@ uploaded_file = st.file_uploader("Upload a CSV file containing keywords:", type=
 
 if uploaded_file is not None and api_key:
     keywords_df = pd.read_csv(uploaded_file)
+    
+    #function to clean up CSV
+    columns_to_keep = ["Keyword", "Volume", "Current URL"]
+    df_new = keywords_df[columns_to_keep]
+    
+   
+   # if st.button("Generate Descriptions"):
+   #    st.write("Generating NLP-friendly descriptions for your keywords...")
 
-    if st.button("Generate Descriptions"):
-        st.write("Generating NLP-friendly descriptions for your keywords...")
+    #    descriptions = []
 
-        descriptions = []
+     #   for keyword in keywords_df['Keyword']:
+       #     description = generate_description(keyword)
+      #      descriptions.append(description)
 
-        for keyword in keywords_df['Keyword']:
-            description = generate_description(keyword)
-            descriptions.append(description)
-
-        keywords_df['description'] = descriptions
+        #keywords_df['description'] = descriptions
 
         # Removed st.write(keywords_df) to stop displaying the results in a table
 
         st.download_button(
             label="Download CSV with Descriptions",
-            data=keywords_df.to_csv(index=False).encode('utf-8'),
-            file_name="keywords_with_descriptions.csv",
+            data=df_new.to_csv(index=False).encode('utf-8'),
+            file_name="keywords_with_two.csv",
             mime="text/csv",
         )
