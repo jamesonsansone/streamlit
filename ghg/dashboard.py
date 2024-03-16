acc668846641905813b1975c5b5771cb2ed591cf
jamesonsansone <38871626+jamesonsansone@users.noreply.github.com>
2024-03-16 14:56:49 -0400

    Update dashboard.py

--- ghg/dashboard.py
+++ ghg/dashboard.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
 # Placeholder functions for CO2e emissions
 def get_co2e_from_energy(energy_consumption):
